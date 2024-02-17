b39d7b1d26eabb09b7760c0b0de0f2ced80a3db1
Demchenko Vladislav <0958587521v@gmail.com>
2024-02-17 15:21:47 +0200

    new method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,8 @@ package org.example;
 
 import org.example.entity.Account;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,6 +21,7 @@ public class Main {
                 .filter(id -> id > 10 && id % 2 == 0)
                 .reduce(Long::sum);
     }
+
     //todo: Given a list of lines. Find the longest string that starts with a vowel.
     public Optional<String> findLongestFirstNameThatStartWithVowelLetter(){
         List<String> listVowelLetter = Arrays.asList("a", "e", "и", "і", "о", "у", "є", "ю", "я");
@@ -35,5 +38,17 @@ public class Main {
                 .sorted()
                 .limit(5)
                 .collect(Collectors.toList());
+    }
+
+    //todo: Find all users who have a balance greater than the average balance of all users.
+    public List<Account> findTheRichestsAccounts(){
+        BigDecimal averageBalance = accounts.stream()
+                .map(Account::getBalance)
+                .reduce(BigDecimal.ZERO, BigDecimal::add)
+                .divide(BigDecimal.valueOf(accounts.size()), RoundingMode.HALF_UP);
+
+        return accounts.stream()
+                .filter(account -> account.getBalance().compareTo(averageBalance) > 0)
+                .toList();
     }
 }
